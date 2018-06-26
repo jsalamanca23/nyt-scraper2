b9990ec134440e246f295b8591a5cd04d4bd76ee
Admin <admin@Admins-MacBook-Pro.local>
2018-06-26 13:06:29 -0700

    merge error

--- nyt-scraper/client/src/pages/Articles/Articles.js
+++
@@ -1,5 +0,0 @@
-import React, { Component } from "react";
-import API from "../../utils/API";
-import Articles from "../../components/Articles";
-import Details from "../../components/Details";
-import NoMatch from "../../cpomponents/NoMatch";
